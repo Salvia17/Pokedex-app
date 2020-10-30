296bd146ef4cec4f9719db26635f2ab9fc2da9e0
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-10-30 18:50:12 +0000

    Adding favorite food variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,11 @@
+//Creates an allert message when opening the page
+alert('Hello world');
 //let sets the variable, myName names it, Salvija is the value of that variable
 let myName = 'Salvija';
 document.write(myName);
-//by writting name of the variable here we give it a new value
-myName = 'Jane Doe';
-document.write(myName);
+
+let favoriteFood = 'Steak and salad';
+document.write(favoriteFood);
 
 let simpleAddition = 2 + 2;
 document.write(simpleAddition);
