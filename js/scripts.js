61d6165ca09d1d14376b5779656b479e9145fc95
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-10-30 18:43:19 +0000

    File name change

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+//let sets the variable, myName names it, Salvija is the value of that variable
+let myName = 'Salvija';
+document.write(myName);
+//by writting name of the variable here we give it a new value
+myName = 'Jane Doe';
+document.write(myName);
+
+let simpleAddition = 2 + 2;
+document.write(simpleAddition);
+
+let size = 100;
+let doubleSize = size * 2;
+let minSize = (doubleSize * 2) - (size / 2);
+document.write(minSize);
