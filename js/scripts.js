7674e8b5cc0312b69c3dee1c2050778e7aaa67f5
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-11-06 13:45:08 +0000

    Fourth pokemon object

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 let pokemonList = [
   {name: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water']},
   {name: 'Jigglypuff', height: 0.5, types: ['Fairy', 'Normal']},
-  {name: 'Psyduck', height: 0.8, types: ['Water']}
+  {name: 'Psyduck', height: 0.8, types: ['Water']},
+  {name: 'Starmie', height: 1.1, types: ['Psychic', 'Water']}
 ];
