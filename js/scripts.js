4b6184443b4a8f86eaf97198bc15e8c4555cd4c8
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-11-06 13:15:57 +0000

    Deleted unrelated code

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,1 @@
-//Creates an allert message when opening the page
-alert('Hello world');
-//let sets the variable, myName names it, Salvija is the value of that variable
-let myName = 'Salvija';
-document.write(myName);
 
-let favoriteFood = 'Steak and salad';
-document.write(favoriteFood);
-
-let simpleAddition = 2 + 2;
-document.write(simpleAddition);
-
-let size = 100;
-let doubleSize = size * 2;
-let minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
