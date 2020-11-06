e4ae2de67f7c5f5266d715b4a4fe3d630cfbe224
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-11-06 13:41:53 +0000

    Third pokemon object created

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
 let pokemonList = [
   {name: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water']},
   {name: 'Jigglypuff', height: 0.5, types: ['Fairy', 'Normal']},
-  {name: '', height: , types: ['', '']}
+  {name: 'Psyduck', height: 0.8, types: ['Water']}
 ];
