5c449bb73ee82a753e493c864e19b44066d0bfca
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-11-06 13:36:19 +0000

    First pokemon object created

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,5 @@
-let pokemonList = [];
+let pokemonList = [
+  {name: 'Slowpoke', height: 1.2, types: ['Psychic', 'Water']},
+  {name: '', height: , types: ['', '']},
+  {name: '', height: , types: ['', '']}
+];
