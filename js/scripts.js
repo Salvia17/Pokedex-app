25fa904b767e090d0c63c3f66e921a5998cc1857
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-11-07 16:17:39 +0000

    Update scripts.js
    Updated the way the array can be written

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,31 @@
-let pokemonList = [
-  {
+const pokemonList = [];
+
+  pokemonList.push({
     name: 'Slowpoke',
     height: 1.2,
     types: ['Psychic', 'Water']
-  },
-  {
+  });
+
+  pokemonList.push({
     name: 'Jigglypuff',
     height: 0.5,
     types: ['Fairy', 'Normal']
-  },
-  {
+  });
+
+  pokemonList.push({
     name: 'Psyduck',
     height: 0.8,
     types: ['Water']
-  },
-  {
+  });
+
+  pokemonList.push({
     name: 'Starmie',
     height: 1.1,
     types: ['Psychic', 'Water']
-  },
-  {
+  });
+
+  pokemonList.push({
     name: 'Charizard',
     height: 1.7,
     types: ['Fire', 'Flying']
-  },
-];
+  });
