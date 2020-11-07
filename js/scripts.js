a7dd6fa1f8116f000939cc38492ab6797b87d8a4
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-11-07 18:00:19 +0000

    Revert "Update scripts.js"
    This reverts commit 25fa904b767e090d0c63c3f66e921a5998cc1857.

--- js/scripts.js
+++ js/scripts.js
@@ -1,31 +1,27 @@
-const pokemonList = [];
-
-  pokemonList.push({
+let pokemonList = [
+  {
     name: 'Slowpoke',
     height: 1.2,
     types: ['Psychic', 'Water']
-  });
-
-  pokemonList.push({
+  },
+  {
     name: 'Jigglypuff',
     height: 0.5,
     types: ['Fairy', 'Normal']
-  });
-
-  pokemonList.push({
+  },
+  {
     name: 'Psyduck',
     height: 0.8,
     types: ['Water']
-  });
-
-  pokemonList.push({
+  },
+  {
     name: 'Starmie',
     height: 1.1,
     types: ['Psychic', 'Water']
-  });
-
-  pokemonList.push({
+  },
+  {
     name: 'Charizard',
     height: 1.7,
     types: ['Fire', 'Flying']
-  });
+  },
+];
