718575291e6f23452fed2569459d71aec5f7a2ef
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-11-07 19:12:29 +0000

    Adjusting line break appropriately

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,11 @@ let pokemonList = [
   types: ['Fairy', 'Normal']
 },
 {
+  name: 'Charizard',
+  height: 1.7,
+  types: ['Fire', 'Flying']
+},
+{
   name: 'Psyduck',
   height: 0.8,
   types: ['Water']
@@ -19,16 +24,13 @@ let pokemonList = [
   height: 1.1,
   types: ['Psychic', 'Water']
 },
-{
-  name: 'Charizard',
-  height: 1.7,
-  types: ['Fire', 'Flying']
-},
 ];
 
 for (let i=0; i < pokemonList.length; i++){
-  document.write (pokemonList[i].name, ' ' + '(', pokemonList[i].height, ')', '</br>');
+  document.write (pokemonList[i].name, ' ' + '(height: ', pokemonList[i].height, ')');
   if (pokemonList[i].height > 1.3){
-    document.write (' WOW! That\'s big!')
+    document.write (' - WOW! That\'s big!' + '</br>', '</br>');
+  }else {
+    document.write ('</br>', '</br>')
   }
 }
