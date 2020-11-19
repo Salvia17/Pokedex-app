2c0c65f53801e3fc9f84f9342ab78407ad371398
Salvia17 <72973751+Salvia17@users.noreply.github.com>
2020-11-19 11:43:48 +0000

    Updated 'for' loop to for.Each function

--- js/scripts.js
+++ js/scripts.js
@@ -2,17 +2,17 @@ let pokemonList = [
 {
   name: 'Slowpoke',
   height: 1.2,
-  types: ['Psychic', 'Water']
+  types: ['Psychic', ' Water']
 },
 {
   name: 'Jigglypuff',
   height: 0.5,
-  types: ['Fairy', 'Normal']
+  types: ['Fairy', ' Normal']
 },
 {
   name: 'Charizard',
   height: 1.7,
-  types: ['Fire', 'Flying']
+  types: ['Fire', ' Flying']
 },
 {
   name: 'Psyduck',
@@ -22,16 +22,29 @@ let pokemonList = [
 {
   name: 'Starmie',
   height: 1.1,
-  types: ['Psychic', 'Water']
+  types: ['Psychic', ' Water']
 },
 ];
 
-for (let i=0; i < pokemonList.length; i++){
-  document.write (pokemonList[i].name, ' ' + '(height: ', pokemonList[i].height, ')');
-  if (pokemonList[i].height > 1.3){
-    document.write (' - WOW! That\'s big!' + '</br>', '</br>');
+pokemonList.forEach(function(pokemon) {
+  if (pokemon.height > 1.3) {
+    document.write (
+      `Name: ${pokemon.name} (type: ${pokemon.types}), (height: ${pokemon.height}) - WOW! That's big! <br><br>`
+    );
+  } else {
+      document.write (
+        `Name: ${pokemon.name} (type: ${pokemon.types}), (height: ${pokemon.height}).<br><br>`
+      );
+    }
+  });
+
+
+//for (let i=0; i < pokemonList.length; i++){
+  //document.write (pokemonList[i].name, ' ' + '(height: ', pokemonList[i].height, ')');
+  //if (pokemonList[i].height > 1.3){
+  //  document.write (' - WOW! That\'s big!' + '</br>', '</br>');
     //adding line break after each pokemon, that's without the phrase WOW that's big//
-  }else {
-    document.write ('</br>', '</br>')
-  }
-}
+//  }else {
+  //  document.write ('</br>', '</br>')
+//  }
+//}
